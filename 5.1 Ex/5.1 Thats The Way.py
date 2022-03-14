d7771b254b6a4c119e968ee51cf330fe50898ee9
Michal Bucks <michall1900@gmail.com>
2022-03-14 08:54:55 +0200

    Add gitignore file and finish 5.1 'Thats The Way' ex

--- 5.1 Ex/5.1 Thats The Way.py
+++ 5.1 Ex/5.1 Thats The Way.py
@@ -1,1 +1,24 @@
-print("hey")
+import os
+FILES_PREFIX = "deep"
+
+
+def find_files_at_path_with_wanted_prefix(path='.', prefix=FILES_PREFIX):
+    """
+    The function receives path to search in and prefix to search files with that prefix.
+    The function return list of files name's that starting with thw wanted prefix.
+    Note: I searched and find the solution at this link:
+    https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
+
+    @param path: String, for search at wanted path.
+    @param prefix: String, for search files with that prefix
+    @return: List of files name's that their prefix is as wanted that existing in wanted path.
+    """
+    files_list_with_wanted_suffix = [file for file in os.listdir(path) if file.startswith(prefix)]
+    return files_list_with_wanted_suffix
+
+
+wanted_path = input("Please Enter wanted path: ")
+files_list = find_files_at_path_with_wanted_prefix(path=wanted_path)
+print("The list is:")
+for file_name in files_list:
+    print(file_name)
