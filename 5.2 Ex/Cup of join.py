dbc0ec2cc0487b6fcae012ad6f830c7e5ee0e0ee
Michal Bucks <michall1900@gmail.com>
2022-03-15 19:44:47 +0200

    Fix mistaked from last pull request

--- 5.2 Ex/Cup of join.py
+++ 5.2 Ex/Cup of join.py
@@ -1,14 +1,14 @@
-"""
-Cup of join
-This program receives from user lists and separator, join the lists to one,
+"""Cup of join
+Receives from user lists and separator, return the lists into one,
 separate them with the wanted separator (or '-' if separator doesn't receive)
 and print the list.
 """
 
+from typing import Any
 
-def join(*tuple_of_lists, sep='-'):
-    """
-    This function receives tuple of lists, join them to one list and put the
+
+def join(sep: Any() = '-', *tuple_of_lists: tuple) -> list or None:
+    """Receives tuple of lists, join them into one list and put the
     separator character to separate them.
     If there is no lists, the function return None.
     If there is no separate character, the function put '-' between lists.
@@ -29,14 +29,18 @@ def join(*tuple_of_lists, sep='-'):
     return result_list
 
 
-lists = []
-temp_str = input("Enter A list. To finish, just press enter: ")
-while len(temp_str):
-    lists.append(temp_str.split())
+def main_cup_of_join():
+    lists = []
     temp_str = input("Enter A list. To finish, just press enter: ")
-separator = input("Enter A separator. If you don't want, just press enter: ")
-if len(separator):
-    print(join(*lists, sep=separator))
-else:
-    print(join(*lists))
+    while len(temp_str):
+        lists.append(temp_str.split())
+        temp_str = input("Enter A list. To finish, just press enter: ")
+    separator = input("Enter A separator. If you don't want, just press enter: ")
+    if len(separator):
+        print(join(*lists, sep=separator))
+    else:
+        print(join(*lists))
 
+
+if __name__ == "__main__":
+    main_cup_of_join()
