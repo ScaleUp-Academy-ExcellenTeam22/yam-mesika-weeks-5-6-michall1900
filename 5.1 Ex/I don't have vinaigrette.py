621587634806fbcd0834366d048bf140e999a671
Michal Bucks <michall1900@gmail.com>
2022-03-27 21:09:22 +0300

    Change return of function rand_time, rename some of functions and variables, put new function for receiving data from user, fix documantation, put if else in one line

--- 5.1 Ex/I don't have vinaigrette.py
+++ 5.1 Ex/I don't have vinaigrette.py
@@ -1,64 +1,58 @@
-"""Receives from user two different dates in format YYYY-MM-DD,
-pick random date between the given dates and check if the weak day of
-the random date is monday.
-If it is, monday is the date that I don't have vinaigrette.
-When the date is on monday, the program print that I don't have vinaigrette,
-otherwise it print that I have vinaigrette.
-
-"""
-
 from datetime import datetime, timedelta
 from random import randrange
 
 
-TIME_FORMAT = "%Y-%m-%d"
+DATE_FORMAT = "%Y-%m-%d"
 
 
-def rand_time(first_date: datetime, second_date: datetime, date_format: str) -> str:
-    """Picking a randomize date in range [first date-second date].
+def random_date(starting_date: datetime, ending_date: datetime) -> datetime:
+    """
+    Picking a randomize date in range [starting date, ending date].
     I got help from
     https://stackoverflow.com/questions/553303/generate-a-random-date-between-two-other-dates
-    :param first_date: first time type for tip of range.
-    :param second_date: second time type for second tip of range.
-    :param date_format: the wanted date print format.
-    :return: date's string format as constant TIME_FORMAT.
+    :param starting_date: Starting datetime to peek random date between it to ending date.
+    :param ending_date: Ending datetime to peek random date between it to starting date (needs to be after
+                        starting date).
+    :return: datetime .
     """
-    if first_date.date() == second_date.date():
-        return first_date.strftime(date_format)
     # picking random seconds in range [first_date.total_seconds, second_date.total_seconds]
-    random_dif_sec = randrange(abs(first_date - second_date).total_seconds()+86400)
-    return (min(first_date, second_date) + timedelta(seconds=random_dif_sec)).strftime(date_format)
+    random_sec = randrange(abs(ending_date - starting_date).total_seconds()+86400)
+    return starting_date + timedelta(seconds=random_sec)
 
 
-def is_date_in_week_day(date: str, day: int) -> bool:
-    """Check if the current date is on the wanted weak day.
+def is_date_in_week_day(date: datetime, day: int) -> bool:
+    """
+    Check if the current date is on the wanted weak day.
     I got help from:
     https://www.delftstack.com/howto/python/python-datetime-day-of-week/
-    :param date: string, the wanted date to check its weak day
-    :param day: int (monday=0, sunday=6.), for check if the date is pointing
-                on that day.
+    :param date: The wanted date to check its weak day
+    :param day: int (monday=0, sunday=6.). For check if the date is pointing on that day.
     :return: true if week day of date same to the given day
     """
-    return datetime.strptime(date, TIME_FORMAT).weekday() == day
+    return date.weekday() == day
 
 
-def main_i_dont_have_v():
+def receive_dates_return_random_date_between() -> datetime:
+    """
+    Receive from user two dates in format -YYYY-MM-DD and return date between them.
+    :return: datetime - Between two dates.
+    """
+    starting_date = datetime.strptime(input("Please enter your starting date in format YYYY-MM-DD: "), DATE_FORMAT)
+    ending_date = datetime.strptime(input("Please enter your ending date in format YYYY-MM-DD: "), DATE_FORMAT)
+    return random_date(starting_date, ending_date)
 
-    # Receives dates from user
-    first_date_str = input("Please enter your first date in format YYYY-MM-DD: ")
-    first_date = datetime.strptime(first_date_str, TIME_FORMAT)
-    second_date_str = input("Please enter your second date in format YYYY-MM-DD: ")
-    second_date = datetime.strptime(second_date_str, TIME_FORMAT)
 
+def main_i_dont_have_vinaigrette() -> None:
+    """
+    Receive from user two dates and, pick randomize date between them and chek if the date is monday.
+    If it is, it print "I don't have vinaigrette :(", else it print "I have vinaigrette :)".
+    :return: None.
+    """
     # Receives random date on given dates range
-    date_picked = rand_time(first_date, second_date, TIME_FORMAT)
-    print(f"At date {date_picked} ", end="")
-    # Check if it is monday and print result.
-    if is_date_in_week_day(date_picked, 0):
-        print("I don't have vinaigrette :( ")
-    else:
-        print("I have vinaigrette :) !!")
+    date_picked = receive_dates_return_random_date_between()
+    print(f"At date {date_picked.strftime(DATE_FORMAT)} ", end="")
+    print("I don't have vinaigrette :( ") if is_date_in_week_day(date_picked, 0) else print("I have vinaigrette :) !!")
 
 
 if __name__ == "__main__":
-    main_i_dont_have_v()
+    main_i_dont_have_vinaigrette()
