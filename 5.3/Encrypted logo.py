7641705ef94dc3655087f65404b644a233dfd6b4
Michal Bucks <michall1900@gmail.com>
2022-03-27 16:43:29 +0300

    Change from list comperhension to for loop because of comment on another program.

--- 5.3/Encrypted logo.py
+++ 5.3/Encrypted logo.py
@@ -7,8 +7,8 @@ def find_encrypted_lines_generator(encrypted_binary_file: BinaryIO) -> str:
     Generator that return all secrete lines in the encrypted file.
     The meaning of secrete line is a line within the file that contains only lowercase
     letters, ending with ! and whose length is >=5.
-    :param encrypted_binary_file to find and decrypt secrete lines
-    :return: secrete lines
+    :param encrypted_binary_file to find and decrypt secrete lines.
+    :return: String of secrete lines.
     """
     for line in encrypted_binary_file:
         for match in findall("([a-z]{5}[a-z]*!)", line.decode(errors='ignore')):
@@ -17,7 +17,8 @@ def find_encrypted_lines_generator(encrypted_binary_file: BinaryIO) -> str:
 
 def main_encrypted_logo() -> None:
     with open("logo.jpg", 'rb') as my_file:
-        [print(secrete_line) for secrete_line in find_encrypted_lines_generator(my_file)]
+        for secrete_line in find_encrypted_lines_generator(my_file):
+            print(secrete_line)
 
 
 if __name__ == "__main__":
