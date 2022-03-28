eef34d26d3ceb15f354363938c7bbe112277b498
Michal Bucks <michall1900@gmail.com>
2022-03-28 13:14:44 +0300

    Delete documantation at starting, remove casting, remove user's test

--- 5.2 Ex/Cup of join.py
+++ 5.2 Ex/Cup of join.py
@@ -1,47 +1,29 @@
-"""Cup of join
-Receives from user lists and separator, return the lists into one,
-separate them with the wanted separator (or '-' if separator doesn't receive)
-and print the list.
-"""
-
 from functools import reduce
 
 
-def join(*tuple_of_lists: tuple, sep: str = '-') -> list or None:
+def join(*tuple_of_lists: list, sep: str = '-') -> list:
     """Receives tuple of lists, join them into one list and put the
     separator character to separate between them.
-    If there is no lists, the function return None.
+    If there is no lists, the function return empty one.
     If there is no separate character, the function put '-' between lists.
     I used this website:
     https://stackoverflow.com/questions/24116865/how-to-join-list-of-lists-with-separator-in-python
-    :param tuple_of_lists: tuple of lists to join them into one
-    :param sep: separate character to separate between each two different lists.
-    :return: new list after join and separate lists.
+    :param tuple_of_lists: Tuple of lists to join them into one.
+    :param sep: Separate character to separate between each two different lists.
+    :return: New list after join and separate lists.
     """
-    # If lists is empty
-    if len(tuple_of_lists) == 0:
-        return None
-    return list(reduce(lambda lst1, lst2: lst1 + [sep] + lst2, tuple_of_lists))
+    return reduce(lambda lst1, lst2: lst1 + ([sep] if len(lst1) != 0 else []) + lst2, tuple_of_lists, [])
 
 
-def main_cup_of_join():
+def main_cup_of_join() -> None:
+    """
+    Using join function to add between lists, separate them with wanted character and print result.
+    :return: None.
+    """
     # Yam Mesika tests
-    print(join([1, 2], [8], [9, 5, 6]))
-    print(join([1, 2], [8], [9, 5, 6], sep='@'))
+    (lambda lists: [print(join(*lists)), print(join(*lists, sep='@'))])([[1, 2], [8], [9, 5, 6]])
     print(join([1]))
     print(join())
-
-    # User choose lists
-    lists = []
-    temp_str = input("Enter A list. To finish, just press enter: ")
-    while len(temp_str):
-        lists.append(temp_str.split())
-        temp_str = input("Enter A list. To finish, just press enter: ")
-    separator = input("Enter A separator. If you don't want, just press enter: ")
-    if len(separator) != 0:
-        print(join(*lists, sep=separator))
-    else:
-        print(join(*lists))
 
 
 if __name__ == "__main__":
